fc26c498603fe2cc1e54ac2c081db5520712adbc
PhetaBots7002 <s.miller@teameureka.net>
2017-11-17 08:45:01 -0700

    Took out some speed at the start
    Shane says robot moves a little after he stopped using joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -159,7 +159,7 @@ public class MecanumTeleOp extends OpMode {
      * the robot more precisely at slower speeds.
      */
     double scaleInput(double dVal)  {
-        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+        double[] scaleArray = { 0.0, 0.10, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
 
         // get the corresponding index for the scaleInput array.
