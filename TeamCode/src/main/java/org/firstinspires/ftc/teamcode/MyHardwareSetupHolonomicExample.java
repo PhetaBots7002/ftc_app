0b24bca62e3908ee172c7a41b2a2b04403397866
ftcTeam7002 <ftc7002@teameureka.net>
2018-10-12 10:32:41 -0600

    HoloTest files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyHardwareSetupHolonomicExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyHardwareSetupHolonomicExample.java
@@ -1,14 +1,13 @@
-package org.firstinspires.ftc.teamcode.LaserSharks;
+package org.firstinspires.ftc.teamcode;
+
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+
 
 /**
- *  CHANGE THIS COMMENTING TO MATCH WHAT THIS PROGRAM IS FOR YOU !!!!!!
- *
  * Created by TeameurekaRobotics on 12/30/2016
+ *
  * This file contains an example Hardware Setup Class for a 4 motor Holonomic drive.
  *
  * It can be customized to match the configuration of your Bot by adding/removing hardware, and then used to instantiate
@@ -20,42 +19,43 @@ import com.qualcomm.robotcore.hardware.Servo;
  *
  */
 
-public class SharkHardWare {
+public class MyHardwareSetupHolonomicExample {
 
    /* Declare Public OpMode members.
     *these are the null statements to make sure nothing is stored in the variables.
     */
 
-    //holonomic motors
+    //Drive motors
     public DcMotor motorFrontRight = null;
     public DcMotor motorFrontLeft = null;
     public DcMotor motorBackRight = null;
     public DcMotor motorBackLeft = null;
 
-    //Arm motors
-    public DcMotor motorLift = null;
+    //Accessories motors
+    //public DcMotor armMotor = null;
 
-    //Arm Servos
-    public Servo servoR = null;
-    public Servo servoL = null;
+    //servos
+        //Add servos here
+
+    //sensors
+        //Add sensors here
 
     /* local OpMode members. */
     HardwareMap hwMap        = null;
 
     //Create and set default servo positions & MOTOR STOP variables.
     //Possible servo values: 0.0 - 1.0  For CRServo 0.5=stop greater or less than will spin in that direction
-    final static double CLOSED = 0.1;
-    final static double OPEN = 0.9;
+    //final static double CLOSED = 0.2;
+    //final static double OPEN = 0.8;
     final static double MOTOR_STOP = 0.0; // sets motor power to zero
+    
     //CR servo variables
-    double SpinLeft = 0.1;
-    double SpinRight = 0.6;
-    double STOP = 0.5;
-   
+        //Add servo variable here
+
    /* Constructor   // this is not required as JAVA does it for you, but useful if you want to add
     * function to this method when called in OpModes.
     */
-    public SharkHardWare() {
+    public MyHardwareSetupHolonomicExample() {
     }
 
     //Initialize standard Hardware interfaces
@@ -66,41 +66,32 @@ public class SharkHardWare {
         /************************************************************
          * MOTOR SECTION
          ************************************************************/
-        // **Define Motors/Servos/Sensors to match Robot Configuration File**
+        // Define Motors to match Robot Configuration File
+        motorFrontLeft = hwMap.dcMotor.get("motorFL");
+        motorFrontRight = hwMap.dcMotor.get("motorFR");
+        motorBackLeft = hwMap.dcMotor.get("motorBL");
+        motorBackRight = hwMap.dcMotor.get("motorBR");
 
-        /// /Holonomic Drive
-        motorFrontRight = hwMap.dcMotor.get("motorFrontRight");
-        motorFrontLeft = hwMap.dcMotor.get("motorFrontLeft");
-        motorBackLeft = hwMap.dcMotor.get("motorBackLeft");
-        motorBackRight = hwMap.dcMotor.get("motorBackRight");
+        //armMotor = hwMap.dcMotor.get ("armMotor");
 
-        //motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE); //may not need reverse
-        //motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
-        //Arm motors
-        motorLift = hwMap.dcMotor.get("motorLift");
-        motorLift.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Set the drive motor directions:
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        //motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        //motorBackRight.setDirection(DcMotor.Direction.REVERSE);
 
         //Keep the motors from moving during initialize.
         motorFrontLeft.setPower(MOTOR_STOP);
         motorFrontRight.setPower(MOTOR_STOP);
         motorBackLeft.setPower(MOTOR_STOP);
         motorBackRight.setPower(MOTOR_STOP);
-        motorLift.setPower(MOTOR_STOP);
-
 
         /************************************************************
          * SERVO SECTION
          ************************************************************/
+
             //Add servo configuration
 
-        servoR = hwMap.servo.get("servoR");
-        servoR.setPosition(CLOSED);
-
-        servoL = hwMap.servo.get("servoL");
-        servoL.setPosition(OPEN);
-        
         /************************************************************
          * SENSOR SECTION
          ************************************************************/
